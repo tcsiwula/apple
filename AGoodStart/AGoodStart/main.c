24d6f5031d112885452b128fa8a95c8f09190171
Timothy Siwula <tcsiwula@dons.usfca.edu>
2014-02-01 21:31:45 -0800

    First commit
    update

---
+++ AGoodStart/AGoodStart/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Tim on 2/1/14.
+//  Copyright (c) 2014 @Tim_Siwula. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
